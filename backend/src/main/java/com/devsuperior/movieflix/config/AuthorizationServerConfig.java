0eb93c51835be37d90c1965651a42e00a13b06a7
Mario Miranda <mario_chan15@hotmail.com>
2022-07-18 11:43:26 -0400

    Implementando CORS

--- backend/src/main/java/com/devsuperior/movieflix/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/movieflix/config/AuthorizationServerConfig.java
@@ -76,7 +76,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.accessTokenConverter(accessTokenConverter)
 			.tokenEnhancer(chain)
 			.userDetailsService(userDetailsService);
-			;
 	}
 	
 }
