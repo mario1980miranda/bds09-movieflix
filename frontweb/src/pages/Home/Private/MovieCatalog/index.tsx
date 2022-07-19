e35f303fb7e7ec35b72f9efea275b6f91548008a
Mario Miranda <mario_chan15@hotmail.com>
2022-07-19 11:43:47 -0400

    MovieCatalog Style e responsividade

--- frontweb/src/pages/Home/Private/MovieCatalog/index.tsx
+++ frontweb/src/pages/Home/Private/MovieCatalog/index.tsx
@@ -1,12 +1,15 @@
+import './styles.css';
 
 const MovieCatalog = () => {
-    return(
-        <div className="container">
-            <h1>Tela listagem de filmes</h1>
-            <a href="/movies/1">Acessar /movies/1</a>
-            <a href="/movies/2">Acessar /movies/2</a>
-        </div>
-    );
+  return (
+    <div className="catalog-container">
+      <h1>Tela listagem de filmes</h1>
+      <div className="catalog-items">
+        <a href="/movies/1">Acessar /movies/1</a>
+        <a href="/movies/2">Acessar /movies/2</a>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCatalog;
