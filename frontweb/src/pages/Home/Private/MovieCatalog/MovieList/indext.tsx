9fbb3170098e96333dd1d6ffd71d11842ff908a4
Mario Miranda <mario_chan15@hotmail.com>
2022-07-26 10:22:25 -0400

    HTML & callback Movies by genre

---
+++ frontweb/src/pages/Home/Private/MovieCatalog/MovieList/indext.tsx
@@ -0,0 +1,68 @@
+import { AxiosRequestConfig } from 'axios';
+import { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Movie } from 'types/movie';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'util/requests';
+
+import './styles.css';
+
+type ControlComponentsData = {
+  activePage: number;
+};
+
+const MovieList = () => {
+  const [controlComponentsData, setControlComponentsData] =
+    useState<ControlComponentsData>({
+      activePage: 0,
+    });
+
+  const [page, setPage] = useState<SpringPage<Movie>>();
+
+  const handlePageChange = (pageNumber: number) => {
+    setControlComponentsData({
+      activePage: pageNumber,
+    });
+  };
+
+  const handleSubmitFilter = () => {};
+
+  const getMoviesByGenre = useCallback(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/movies?genreId=1',
+      params: {
+        page: controlComponentsData.activePage,
+        size: 3,
+      },
+    };
+
+    requestBackend(config)
+      .then((response) => {
+        setPage(response.data);
+      })
+      .catch((error) => {
+        console.log('TOAST', error);
+      });
+  }, [controlComponentsData]);
+
+  useEffect(() => {
+    getMoviesByGenre();
+  }, [getMoviesByGenre]);
+  return (
+    <div className="row">
+      {page?.content.map((movie) => (
+        <Link to={`/movies/${movie.id}`}>
+          <div key={movie.id} className="base-card movie-list-card">
+            <img src={movie.imgUrl} alt={movie.title} />
+            <h1>{movie.title}</h1>
+            <h2>{movie.year}</h2>
+            <p>{movie.subTitle}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
--- frontweb/src/pages/Home/Private/MovieCatalog/index.tsx
+++ frontweb/src/pages/Home/Private/MovieCatalog/index.tsx
@@ -1,12 +1,22 @@
+import MovieList from './MovieList/indext';
 import './styles.css';
 
 const MovieCatalog = () => {
   return (
     <div className="catalog-container">
-      <h1>Tela listagem de filmes</h1>
-      <div className="catalog-items">
-        <a href="/movies/1">Acessar /movies/1</a>
-        <a href="/movies/2">Acessar /movies/2</a>
+      <div className="base-card">
+        <select>
+          <option>Aventura</option>
+        </select>
+      </div>
+      <MovieList />
+      <div>
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>...</li>
+        </ul>
       </div>
     </div>
   );
