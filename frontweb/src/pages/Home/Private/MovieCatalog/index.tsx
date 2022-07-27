f89fe69345debe36d0b173f3339b8fa7c99f9de1
Mario Miranda <mario_chan15@hotmail.com>
2022-07-27 09:11:14 -0400

    Esquema HTML movieList

--- frontweb/src/pages/Home/Private/MovieCatalog/index.tsx
+++ frontweb/src/pages/Home/Private/MovieCatalog/index.tsx
@@ -5,7 +5,76 @@ import './styles.css';
 const MovieCatalog = () => {
   return (
     <div className="catalog-container">
-      <MovieList />
+      <div className="catalog-select-container">
+        <div className="base-card catalog-select-card">
+          <select className="catalog-select">
+            <option value="teste" label="teste" />
+          </select>
+        </div>
+        <div className="row">
+          <div className="col-sm-6 col-lg-6 col-xl-2">
+            <a href="#link">
+              <div className="base-card">
+                <img src="" alt="nome" />
+                <h1>Texto</h1>
+                <h2>2007</h2>
+                <p>sinopsys</p>
+              </div>
+            </a>
+          </div>
+          <div className="col-sm-6 col-lg-6 col-xl-2">
+            <a href="#link">
+              <div className="base-card">
+                <img src="" alt="nome" />
+                <h1>Texto</h1>
+                <h2>2007</h2>
+                <p>sinopsys</p>
+              </div>
+            </a>
+          </div>
+          <div className="col-sm-6 col-lg-6 col-xl-2">
+            <a href="#link">
+              <div className="base-card">
+                <img src="" alt="nome" />
+                <h1>Texto</h1>
+                <h2>2007</h2>
+                <p>sinopsys</p>
+              </div>
+            </a>
+          </div>
+          <div className="col-sm-6 col-lg-6 col-xl-2">
+            <a href="#link">
+              <div className="base-card">
+                <img src="" alt="nome" />
+                <h1>Texto</h1>
+                <h2>2007</h2>
+                <p>sinopsys</p>
+              </div>
+            </a>
+          </div>
+          <div className="col-sm-6 col-lg-6 col-xl-2">
+            <a href="#link">
+              <div className="base-card">
+                <img src="" alt="nome" />
+                <h1>Texto</h1>
+                <h2>2007</h2>
+                <p>sinopsys</p>
+              </div>
+            </a>
+          </div>
+          <div className="col-sm-6 col-lg-6 col-xl-2">
+            <a href="#link">
+              <div className="base-card">
+                <img src="" alt="nome" />
+                <h1>Texto</h1>
+                <h2>2007</h2>
+                <p>sinopsys</p>
+              </div>
+            </a>
+          </div>
+        </div>
+        <h1>1 2 3 ...</h1>
+      </div>
     </div>
   );
 };
